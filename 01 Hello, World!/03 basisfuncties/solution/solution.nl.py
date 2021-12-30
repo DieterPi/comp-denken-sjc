c5d7a4400ad32caf1177ea87c5c8bb26969324ba
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2021-12-31 00:12:21 +0100

    Fixed solution

--- 01 Hello, World!/03 basisfuncties/solution/solution.nl.py
+++ 01 Hello, World!/03 basisfuncties/solution/solution.nl.py
@@ -1,7 +1,9 @@
+# vul de juiste invoer in
 getal1 = int( input( 'Geef het eerste getal in: ' ) )
 getal2 = int( input( 'Geef het tweede getal in: ' ) )
 getal3 = int( input( 'Geef het derde getal in: ' ) )
 
+# vul hieronder de andere uitvoer aan
 print('maximum:', max( getal1, getal2, getal3 ) )
 print('minimum:', min( getal1, getal2, getal3 ) )
 print('gemiddelde:', round( ( getal1 + getal2 + getal3 ) / 3 , 2 ) )
