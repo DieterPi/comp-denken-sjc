88005ac9bfd7228078e91b6eba0a285d9f10a400
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-01-02 14:52:03 +0100

    Solution fix for python3

--- 01 Hello, World!/05 ex bol/solution/solution.nl.py
+++ 01 Hello, World!/05 ex bol/solution/solution.nl.py
@@ -4,6 +4,6 @@ PI = 3.141592
 straal = float(input( 'Geef de straal van de bol in (in cm): ' ))
 
 oppervlakte = 4*PI*pow(straal, 2)
-volume = 4.0/3.0*PI*pow(straal, 3)
+volume = 4/3*PI*pow(straal, 3)
 
 print("\noppervlakte: {} cm²\nvolume: {} cm³".format(round(oppervlakte,2),round(volume,2)))
