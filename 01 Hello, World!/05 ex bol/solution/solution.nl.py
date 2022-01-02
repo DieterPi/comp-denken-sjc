8b82124641d69154084a4544e47478f890b44a79
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-01-02 11:25:44 +0100

    auto generation of test cases fix

--- 01 Hello, World!/05 ex bol/solution/solution.nl.py
+++ 01 Hello, World!/05 ex bol/solution/solution.nl.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 PI = 3.141592
 straal = float(input( 'Geef de straal van de bol in (in cm): ' ))
 
 oppervlakte = 4*PI*pow(straal, 2)
 volume = 4/3*PI*pow(straal, 3)
 
-print( 'oppervlakte:', round( oppervlakte, 2 ), 'cm²')
-print( 'volume:', round( volume, 2 ), 'cm³')
+print("\noppervlakte: {} cm²".format(round(oppervlakte,2)))
+print("volume: {} cm³".format(round(volume,2)))
