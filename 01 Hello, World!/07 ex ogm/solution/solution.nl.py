39818e723fecb655c8013023f0b409da51b691f6
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-01-02 16:19:52 +0100

    fixed pythia solutions

--- 01 Hello, World!/07 ex ogm/solution/solution.nl.py
+++ 01 Hello, World!/07 ex ogm/solution/solution.nl.py
@@ -6,4 +6,4 @@ getal2 = int( input( 'Geef een getal van 4 cijfers in: ' ) )
 getal3 = int( input( 'Geef een getal van 3 cijfers in: ' ) )
 
 output = (getal1 * 10000 * 1000 + getal2 * 1000 + getal3) % 97
-print( 'controlegetal:', output )
+print( '\ncontrolegetal: {}'.format( output ) )
