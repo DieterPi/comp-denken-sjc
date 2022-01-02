d712c788cda988e9476c1272754525a176bf22b0
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-01-02 16:12:26 +0100

    Fixing solution and adding generator

--- 01 Hello, World!/05 ex massadichtheid/solution/solution.nl.py
+++ 01 Hello, World!/05 ex massadichtheid/solution/solution.nl.py
@@ -4,4 +4,4 @@
 m = float(input('Geef de massa in kg in: '))
 V = float(input('Geef het volume in l in: '))
 
-print('massadichtheid:', round( m * 1000 / V, 1) )
+print('\nmassadichtheid: {}'.format(round( m / V, 2) ) )
