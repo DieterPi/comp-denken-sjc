9edda867d9408c246c731f49616b21db91e6128c
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-01-03 17:19:44 +0100

    Indentation fix

--- 3th year/03 Geavanceerde datatypes/xx ex eratosthenes/solution/solution.nl.py
+++ 3th year/03 Geavanceerde datatypes/xx ex eratosthenes/solution/solution.nl.py
@@ -5,13 +5,13 @@ lijst = list( range( 1, max + 1 ) )
 
 lijst[0] = 0
 for i in lijst:
-  if i != 0:
-    for k in range( 2, max // i + 1 ):
-      lijst[i * k - 1] = 0
+    if i != 0:
+        for k in range( 2, max // i + 1 ):
+            lijst[i * k - 1] = 0
 
 priemgetallen = []
 for getal in lijst:
-  if getal != 0:
-    priemgetallen.append( getal )
+    if getal != 0:
+        priemgetallen.append( getal )
 
 print('\n{}'.format(priemgetallen) )
