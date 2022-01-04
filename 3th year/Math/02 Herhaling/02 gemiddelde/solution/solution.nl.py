6f5b7534ed0f38e6149bb9aa5b8e77c35cf49766
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-01-04 16:30:44 +0100

    Indentation fix

--- 3th year/Math/02 Herhaling/02 gemiddelde/solution/solution.nl.py
+++ 3th year/Math/02 Herhaling/02 gemiddelde/solution/solution.nl.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 
 def rekenkundig_gemiddelde( tupel ):
-  som = 0
-  for i in range( len( tupel ) ):
-    som += tupel[i]
-  return round( som / len( tupel ), 2 )
+    som = 0
+    for i in range( len( tupel ) ):
+        som += tupel[i]
+    return round( som / len( tupel ), 2 )
 
 def harmonisch_gemiddelde( tupel ):
-  som = 0
-  for i in range( len( tupel ) ):
-    som += 1 / tupel[i]
-  return round( len( tupel ) / som, 2 )
+    som = 0
+    for i in range( len( tupel ) ):
+        som += 1 / tupel[i]
+    return round( len( tupel ) / som, 2 )
 
 if __name__ == '__main__':
     import doctest
