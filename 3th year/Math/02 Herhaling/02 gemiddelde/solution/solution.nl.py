69e60fe8cea0f2d6353def563dcd3b42d2612e6c
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-01-05 22:48:49 +0100

    Better solution

--- 3th year/Math/02 Herhaling/02 gemiddelde/solution/solution.nl.py
+++ 3th year/Math/02 Herhaling/02 gemiddelde/solution/solution.nl.py
@@ -3,14 +3,14 @@
 
 def rekenkundig_gemiddelde( tupel ):
     som = 0
-    for i in range( len( tupel ) ):
-        som += tupel[i]
+    for getal in tupel:
+        som += getal
     return round( som / len( tupel ), 2 )
 
 def harmonisch_gemiddelde( tupel ):
     som = 0
-    for i in range( len( tupel ) ):
-        som += 1 / tupel[i]
+    for getal in tupel:
+        som += 1 / getal
     return round( len( tupel ) / som, 2 )
 
 if __name__ == '__main__':
