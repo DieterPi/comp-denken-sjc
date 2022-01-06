f988edaf5d57bf082245ada227a7914fee906663
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-01-07 00:53:00 +0100

    Initial favoriete kleur exercise

---
+++ 3th year/Math/02 Herhaling/99 exercises/12 lijsten/description/description.nl.md
@@ -0,0 +1,12 @@
+## Opgave
+Beschouw onderstaande lijst opgebouwd uit tupels van leerlingen en hun favoriete kleur.
+
+Schrijf een functie `favoriete_kleur()` met een kleur als parameter en die retourneert hoeveel leerlingen die kleur als favoriete kleur hebben.
+
+#### Voorbeeld
+```
+>>> favoriete_kleur('rood')
+8
+>>> favoriete_kleur('oranje')
+12
+```
---
+++ 3th year/Math/02 Herhaling/99 exercises/12 lijsten/solution/solution.nl.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+lijst = [('Alixe', 'indigo'), ('Anthe', 'groen'), ('Cara', 'geel'), ('Célia', 'oranje'), ('Dionah', 'violet'), ('Divine', 'geel'), ('Dorcas', 'indigo'), ('Eliana', 'geel'), ('Eliane', 'groen'), ('Elien', 'oranje'), ('Eline', 'indigo'), ('Elise', 'violet'), ('Emma', 'oranje'), ('Feliz', 'indigo'), ('Fiebe', 'blauw'), ('Fleur', 'oranje'), ('Fran', 'rood'), ('Geike', 'violet'), ('Grace', 'indigo'), ('Griet', 'geel'), ('Guilia', 'oranje'), ('Hannah', 'violet'), ('Hanne', 'groen'), ('Hayley', 'rood'), ('Heleen', 'oranje'), ('Helena', 'indigo'), ('Inès', 'geel'), ('Iris', 'geel'), ('Isolde', 'violet'), ('Jade', 'indigo'), ('Jinte', 'blauw'), ('Jitske', 'groen'), ('Julia', 'rood'), ('Julie', 'groen'), ('Kawtar', 'violet'), ('Kyra', 'blauw'), ('Lien', 'violet'), ('Lina', 'oranje'), ('Linde', 'oranje'), ('Lisa', 'oranje'), ('Lore', 'rood'), ('Lotte', 'groen'), ('Mareta', 'violet'), ('Marie', 'oranje'), ('Marthe', 'geel'), ('Méira', 'geel'), ('Mira', 'groen'), ('Myrthe', 'indigo'), ('Naya', 'blauw'), ('Nele', 'groen'), ('Nette', 'indigo'), ('Nienke', 'rood'), ('Nina', 'groen'), ('Noor', 'violet'), ('Renée', 'geel'), ('Sara', 'rood'), ('Sofie', 'blauw'), ('Sosena', 'violet'), ('Stella', 'oranje'), ('Stien', 'blauw'), ('Stina', 'groen'), ('Thalia', 'oranje'), ('Tille', 'rood'), ('Tinka', 'indigo'), ('Xanthe', 'blauw'), ('Yana', 'geel'), ('Yente', 'rood'), ('Yorunn', 'violet')]
+
+def favoriete_kleur( kleur ):
+    aantal = 0
+    for item in lijst:
+        aantal += item[1] == kleur
+    
+    return aantal
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
