97dc74ee873c9557a0fed560bbf5b02e5731112f
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-01-30 15:30:02 +0100

    Solution cleanup

--- 3th year/ICT/01 Hello, World!/99 exercises/18 caesarrotatie/solution/solution.nl.py
+++ 3th year/ICT/01 Hello, World!/99 exercises/18 caesarrotatie/solution/solution.nl.py
@@ -6,4 +6,4 @@ alfabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 letter = input( 'Geef een hoofdletter in: ' )
 pos = alfabet.find(letter)
 
-print( '\n{}'.format( alfabet[ ( pos + 13 ) % 26 ] ) )
+print( '{}'.format( alfabet[ ( pos + 13 ) % 26 ] ) )
