0a452d225e683258b9fc84ab1a16ee17b88a07e1
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-05-25 00:13:58 +0200

    Initial Bond exercise

---
+++ 3th year/Math/01 Selectie/99 exercises/10 bond/solution/solution.nl.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+naam = str(input('Geef je naam in: '))
+print('\n')
+if ( naam == 'Bond') :
+    print( 'Welkom 007' )
+else:
+    print( 'Goeiemorgen', naam)
