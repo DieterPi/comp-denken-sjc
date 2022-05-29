60251bd524862051e17dd3e1c3b36381fd0e06c2
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-05-29 19:16:38 +0200

    Updated solution

--- 3th year/Math/99 tests/06 bpost/solution/solution.nl.py
+++ 3th year/Math/99 tests/06 bpost/solution/solution.nl.py
@@ -6,17 +6,17 @@ aantal = float( input( 'Geef het aantal in: ' ) )
 
 if aantal >= 10:
     if massa >= 350:
-        bedrag = aantal * 9.30
+        multi = 9.30
     elif massa >= 100:
-        bedrag = aantal * 5.58
+        multi = 5.58
     else:
-        bedrag = aantal * 3.72
+        multi = 3.72
 else:
     if massa >= 350:
-        bedrag = aantal * 9.45
+        multi = 9.45
     elif massa >= 100:
-        bedrag = aantal * 5.67
+        multi = 5.67
     else:
-        bedrag = aantal * 3.78
+        multi = 3.78
 
-print( '\nU dient EUR', round( bedrag, 2), 'te betalen.' )
+print( '\nU dient EUR', round( aantal * multi, 2), 'te betalen.' )
