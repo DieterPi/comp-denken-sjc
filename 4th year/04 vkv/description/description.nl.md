a06fe6bbd3c29788c53b1ce7827469dd76695397
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-09-07 21:52:54 +0200

    Initial vkv exercise

---
+++ 4th year/04 vkv/description/description.nl.md
@@ -0,0 +1,11 @@
+![abc-formule](media/tattoo.jpeg "De abc-formule"){:data-caption="De oplossingen van een vierkantsvergelijking." width="450px"}
+
+## Opgave
+
+Schrijf een functie `vkv( a, b, c )` die voor een vierkantsvergelijking $$ax^2+bx+c=0$$ de oplossingen berekent.
+
+#### Voorbeelden
+```
+>>> vkv(1, 3, -4) 
+Er zijn 2 reële oplossingen, namelijk 1.0 en 1.0
+```
---
+++ 4th year/04 vkv/solution/solution.nl.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import math
+
+def vkv( a, b, c ):
+    D = b**2 - 4*a*c
+    if( D < 0 ):
+        print( 'Er zijn geen reële oplossingen' )
+    elif( D == 0):
+        x = -b / ( 2 * a )
+        print( 'Er is één oplossing, namelijk:', round( x, 2 ) )
+    else:
+        x1 = ( -b + math.sqrt( D ) ) / ( 2 * a )
+        x2 = ( -b + math.sqrt( D ) ) / ( 2 * a )
+        print( 'Er zijn 2 reële oplossingen, namelijk', round( x1, 2 ), 'en', round( x2, 2 ) )
+        
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
