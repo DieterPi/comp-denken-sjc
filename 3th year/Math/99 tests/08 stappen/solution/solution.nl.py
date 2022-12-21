8b1413d6374b13ca2db77e576e3b7f54e434a36e
dvanderfaeillie <dieter.vanderfaeillie@gmail.com>
2022-12-21 11:30:48 +0100

    Solution fix

--- 3th year/Math/99 tests/08 stappen/solution/solution.nl.py
+++ 3th year/Math/99 tests/08 stappen/solution/solution.nl.py
@@ -5,7 +5,7 @@ aantalStappen = int( input( '' ) )
 
 teDoen = 10000 - aantalStappen
 
-if( aantalStappen < 10000 ):
+if( aantalStappen < 9999 ):
     print('\nJe dient nog {} stappen te zetten.'.format( teDoen ) )
 elif (aantalStappen == 9999):
     print('\nJe moet maar één stap meer zetten.')
