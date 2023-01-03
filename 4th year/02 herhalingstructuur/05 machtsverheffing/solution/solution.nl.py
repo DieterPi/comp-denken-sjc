9054c1f2374636ed1474686becd162a0507b2153
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-01-03 21:57:02 +0100

    Solution fix

--- 4th year/02 herhalingstructuur/05 machtsverheffing/solution/solution.nl.py
+++ 4th year/02 herhalingstructuur/05 machtsverheffing/solution/solution.nl.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-n = int( input( 'Geef n = ' ) )
-x = int( input( 'Geef x = ' ) )
+n = int( input( 'n = ' ) )
+x = int( input( 'x = ' ) )
 
 macht = 1
 for i in  range( n ):
     macht *= x
     
-print()
 print( macht ) 
