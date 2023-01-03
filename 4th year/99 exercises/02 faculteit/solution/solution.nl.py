3b212f6b52f42eabb565f44c8ffc01ecd2670a15
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-01-03 22:30:45 +0100

    Judge fix

--- 4th year/99 exercises/02 faculteit/solution/solution.nl.py
+++ 4th year/99 exercises/02 faculteit/solution/solution.nl.py
@@ -5,7 +5,7 @@ def faculteit( getal ):
     product = 1
     for i in range( 1, getal + 1 ):
         product *= i
-    print( product )
+    print( str(product) )
 
 if __name__ == '__main__':
     import doctest
