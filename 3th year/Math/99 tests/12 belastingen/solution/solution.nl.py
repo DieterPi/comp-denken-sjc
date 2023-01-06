7c4224e2860daa102a35a64212a0a174eb2793a7
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-01-06 22:36:12 +0100

    Solution fix

--- 3th year/Math/99 tests/12 belastingen/solution/solution.nl.py
+++ 3th year/Math/99 tests/12 belastingen/solution/solution.nl.py
@@ -7,7 +7,7 @@ def belasting( inkomen ):
     elif inkomen > 23900:
         belasting = ( inkomen - 23900 ) * 0.45 + ( 23900 - 13540 ) * 0.4 + 13540 * 0.25
     elif inkomen > 13540:
-         belasting = ( inkomen - 13540 ) * 0.4 + 13540 * 0.25
+        belasting = ( inkomen - 13540 ) * 0.4 + 13540 * 0.25
     else:
         belasting =  inkomen * 0.25
     
