de3c0bfbe3ebfdb9f65a23c98ecf48d3b1d822ef
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-01-08 21:05:00 +0100

    Removed paratheses

--- 3th year/Math/01 Selectie/99 exercises/16 differentiequötiënt/solution/solution.nl.py
+++ 3th year/Math/01 Selectie/99 exercises/16 differentiequötiënt/solution/solution.nl.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def differentiequotient( koppel1, koppel2 ):
-    if (koppel1[0] != koppel2[0]):
+    if koppel1[0] != koppel2[0]:
         getal = (koppel2[1] - koppel1[1]) / (koppel2[0] - koppel1[0])
         print( 'Het differentiequötiënt op het interval [', min(koppel1[0], koppel2[0]),',',max(koppel1[0], koppel2[0]),'] is:', round( getal, 2))
     else:
