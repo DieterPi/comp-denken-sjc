999ed92607ee2e6d162d5d520df27bea66a2581b
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-05-09 19:59:17 +0200

    Adjusted to print instead of return

--- 3th year/Math/01 Selectie/02 booleaanse expressies/description/boilerplate/boilerplate.nl
+++ 3th year/Math/01 Selectie/02 booleaanse expressies/description/boilerplate/boilerplate.nl
@@ -2,4 +2,4 @@ def deelbaar_door_zes_en_acht( getal ):
     deelbaar_door_zes = 
     deelbaar_door_acht = 
 
-    return 
+    print(  ) 
--- 3th year/Math/01 Selectie/02 booleaanse expressies/solution/solution.nl.py
+++ 3th year/Math/01 Selectie/02 booleaanse expressies/solution/solution.nl.py
@@ -5,7 +5,7 @@ def deelbaar_door_zes_en_acht( getal ):
     deelbaar_door_zes = getal % 6 == 0
     deelbaar_door_acht = getal % 8 == 0
 
-    return deelbaar_door_zes and deelbaar_door_acht
+    print( deelbaar_door_zes and deelbaar_door_acht )
 
 if __name__ == '__main__':
     import doctest
