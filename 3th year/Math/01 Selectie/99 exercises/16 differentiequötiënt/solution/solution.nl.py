fbcf180ad03751debb9be2a056eee632bb380e86
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-05-17 18:27:36 +0200

    Typo fix

--- 3th year/Math/01 Selectie/99 exercises/16 differentiequötiënt/solution/solution.nl.py
+++ 3th year/Math/01 Selectie/99 exercises/16 differentiequötiënt/solution/solution.nl.py
@@ -4,7 +4,7 @@
 def differentiequotient( koppel1, koppel2 ):
     if koppel1[0] != koppel2[0]:
         getal = (koppel2[1] - koppel1[1]) / (koppel2[0] - koppel1[0])
-        print( 'Het differentiequötiënt op het interval [', min(koppel1[0], koppel2[0]),',',max(koppel1[0], koppel2[0]),'] is:', round( getal, 2))
+        print( 'Het differentiequotiënt op het interval [', min(koppel1[0], koppel2[0]),',',max(koppel1[0], koppel2[0]),'] is:', round( getal, 2))
     else:
         print( 'Deze twee punten hebben eenzelfde x coördinaat, hier kan je dus geen differentiequotiënt berekenen' )
 
