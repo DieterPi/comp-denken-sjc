cbfbb0b5cad80be99f332b9356410c3f42560cb4
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-20 18:04:08 +0200

    Testing options and fixes

--- 4th year/04 geavanceerd/15 matrixvermenigvuldiging/solution/solution.nl.py
+++ 4th year/04 geavanceerd/15 matrixvermenigvuldiging/solution/solution.nl.py
@@ -2,19 +2,17 @@
 # -*- coding: utf-8 -*-
 
 def matrixvermenigvuldiging( m1, m2 ):
-    if len(m1[0]) != len(m2):
+    if len( m1[0] ) != len( m2 ):
         result = []
     else:
         result = []
-        for r in range(len(m1)):
+        for r in range( len( m1 ) ):
             row = []
-            for c in range(len(m2[0])):
+            for c in range( len( m2[0] ) ):
                 som = 0
-                for i in range(len(m1[0])):
-                    som += m1[r][i] + m2[i][c]
-                row.append(som)       
-            result.append(row)
+                for i in range( len( m1[0] ) ):
+                    som += m1[r][i] * m2[i][c]
+                row.append( som )
+            result.append( row )
 
-    print(result)
-    
-
+    print( result )
