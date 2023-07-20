1b2fcea664d1d1126294c4447fad8cc93acdb47a
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-20 17:24:08 +0200

    Initial matrix multiplication exercise with TESTed judge

---
+++ 4th year/04 geavanceerd/15 matrixvermenigvuldiging/description/description.nl.md
@@ -0,0 +1,10 @@
+## Opgave
+Schrijf een functie `matrixvermenivuldiging()` die gegeven twee matrices het product uitrekent.
+
+#### Voorbeeld
+```
+>>> diagonaal( 3 )
+ 0  1  2 
+ 1  0  1 
+ 2  1  0 
+```
---
+++ 4th year/04 geavanceerd/15 matrixvermenigvuldiging/solution/solution.nl.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def matrixvermenivuldiging( m1, m2 ):
+    if len(m1[0]) != len(m2):
+        result = []
+    else:
+        result = []
+        for r in range(len(m1)):
+            row = []
+            for c in range(len(m2[0])):
+                som = 0
+                for i in range(len(m1[0])):
+                    som += m1[r][i] + m2[i][c]
+                row.append(som)       
+            result.append(row)
+
+    return result
+    
+
