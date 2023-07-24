d9f7637d527381d4b27de9e2f4307c34d4acd639
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-25 00:52:36 +0200

    Testing final evaluator function

--- 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
+++ 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
@@ -1,9 +1,18 @@
 from evaluation_utils import EvaluationResult, Message
 
-def hoger_lager():
-    return "a"
+def hoger_lager( value, result, aantal ):
+    if value > result:
+        str = "Gok een getal: {}\nLager".format( value )
+    elif value < result:
+        str = "Gok een getal: {}\nHoger".format( value )
+    else: # value == result
+        str = "Gok een getal: {}\nAantal pogingen: {}".format( value, aantal )
+    return str        
 
 def evaluate_value(expected, actual, args):
-    #inputs = args.split('\n')
-    
-    return EvaluationResult(True, expected, actual, [Message( "ab" + args[0] )])
+    inputs = args[0].split('\n')
+    msg = ""
+    for input in inputs:
+        msg += hoger_lager( input, inputs[-1], len( inputs ) )
+
+    return EvaluationResult(True, expected, actual, [Message( msg )])
