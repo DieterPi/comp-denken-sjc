9791da6f33d9c08626a41b83607fb0bab3ee3a40
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-25 00:35:49 +0200

    Evaluator experiments

--- 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
+++ 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
@@ -1,5 +1,9 @@
 from evaluation_utils import EvaluationResult, Message
 
+def hoger_lager():
+    pass
+
 def evaluate_value(expected, actual, args):
+    inputs = args.split('\n')
     
-    return EvaluationResult(True, expected, actual, [Message("Test van de evaluatiefunctie")])
+    return EvaluationResult(True, expected, actual, [Message(actual)])
