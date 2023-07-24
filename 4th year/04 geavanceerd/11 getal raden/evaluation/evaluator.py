dad51ddd67a94b8d3629f777c3d9a70b7e336f62
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-25 00:46:58 +0200

    Trying to fix

--- 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
+++ 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
@@ -1,9 +1,9 @@
 from evaluation_utils import EvaluationResult, Message
 
 def hoger_lager():
-    pass
+    return "a"
 
 def evaluate_value(expected, actual, args):
     #inputs = args.split('\n')
     
-    return EvaluationResult(True, expected, actual, [Message(type( args ))])
+    return EvaluationResult(True, expected, actual, [Message( "ab" + args )])
