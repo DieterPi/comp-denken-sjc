d150baee9ff7540f009ad29f82dd182323c41133
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-25 00:57:42 +0200

    Still trying to fix the evaluator

--- 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
+++ 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
@@ -2,11 +2,11 @@ from evaluation_utils import EvaluationResult, Message
 
 def hoger_lager( value, result, aantal ):
     if value > result:
-        str = "Gok een getal: {}\nLager".format( value )
+        str = "Gok een getal: {}\n Hoger".format( value )
     elif value < result:
-        str = "Gok een getal: {}\nHoger".format( value )
+        str = "Gok een getal: {}\n Lager".format( value )
     else: # value == result
-        str = "Gok een getal: {}\nAantal pogingen: {}".format( value, aantal )
+        str = "Gok een getal: {}\n Aantal pogingen: {}".format( value, aantal )
     return str        
 
 def evaluate_value(expected, actual, args):
@@ -15,4 +15,4 @@ def evaluate_value(expected, actual, args):
     for input in inputs:
         msg += hoger_lager( input, inputs[-1], len( inputs ) )
 
-    return EvaluationResult(True, expected, actual, [Message( msg )])
+    return EvaluationResult(True, expected, actual, [Message( msg +'\n'+expected+'\n'+actual)])
