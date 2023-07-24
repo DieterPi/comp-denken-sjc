d9ad613c983de24cba5d6639fa211511cda552ca
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-24 22:29:51 +0200

    Adapt evaluator function

--- 3th year/ICT/01 Hello, World!/99 exercises/02 zerodivisionerror/evaluation/evaluator.py
+++ 3th year/ICT/01 Hello, World!/99 exercises/02 zerodivisionerror/evaluation/evaluator.py
@@ -2,17 +2,16 @@ import traceback
 
 from evaluation_utils import EvaluationResult, Message
 
-
-def evaluate(value):
-    if isinstance(value, ZeroDivisionError):
+def evaluate_value(expected, actual, args):
+    if isinstance(actual, ZeroDivisionError):
         # If a zero division error, show the stacktrace.
-        formatted = "".join(traceback.format_exception(type(value), value, value.__traceback__))
+        formatted = "".join(traceback.format_exception(type(actual), actual, actual.__traceback__))
         return EvaluationResult(True, formatted, formatted)
-    elif isinstance(value, Exception):
+    elif isinstance(actual, Exception):
         # If another error, show the stacktrace as well.
-        formatted = "".join(traceback.format_exception(type(value), value, value.__traceback__))
+        formatted = "".join(traceback.format_exception(type(actual), actual, actual.__traceback__))
         return EvaluationResult(False, "ZeroDivisionError", formatted, [Message(f"Verwachtte een ZeroDivisionError, maar kreeg een {type(value).__name__}.")])
     else:
         # Else show the str of the value.
-        actual = str(value) if value else ""
+        actual = str(actual) if actual else ""
         return EvaluationResult(False, "ZeroDivisionError", actual, [Message("Verwachtte een ZeroDivisionError.")])
