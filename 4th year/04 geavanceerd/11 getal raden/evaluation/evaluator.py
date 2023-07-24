4b0ecaf06ed4237ebca389b2ac49f64c43227b4d
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-25 00:36:55 +0200

    Fixing evaluator

--- 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
+++ 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
@@ -4,6 +4,6 @@ def hoger_lager():
     pass
 
 def evaluate_value(expected, actual, args):
-    inputs = args.split('\n')
+    #inputs = args.split('\n')
     
     return EvaluationResult(True, expected, actual, [Message(actual)])
