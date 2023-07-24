15eb2e6d721e0e7d401296925ad600fce349bcfc
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-24 16:36:17 +0200

    Solution fix

--- 4th year/04 geavanceerd/16 bisection method/solution/solution.nl.py
+++ 4th year/04 geavanceerd/16 bisection method/solution/solution.nl.py
@@ -11,5 +11,4 @@ def bisectiemethode( f: float, a: float , b: float, toleratie: float) -> float:
         else: # f( c ) == 0
             a = c
             b = c
-        print( round( c, 4 ))
     return round( c, 4 )
