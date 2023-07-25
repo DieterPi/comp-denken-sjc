2178a38eec794db1a9bda325c7e66d22b1a1968e
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-07-25 11:41:56 +0200

    Fixing multiline

--- 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
+++ 4th year/04 geavanceerd/11 getal raden/evaluation/evaluator.py
@@ -2,17 +2,18 @@ from evaluation_utils import EvaluationResult, Message
 
 def hoger_lager( value, result, aantal ):
     if value > result:
-        str = "Gok een getal: {}\\n Hoger".format( value )
+        str = "Gok een getal: {}\n Hoger".format( value )
     elif value < result:
-        str = "Gok een getal: {}\\n Lager".format( value )
+        str = "Gok een getal: {}\n Lager".format( value )
     else: # value == result
-        str = "Gok een getal: {}\\n Aantal pogingen: {}".format( value, aantal )
+        str = "Gok een getal: {}\n Aantal pogingen: {}".format( value, aantal )
     return str        
 
 def evaluate_value(expected, actual, args):
     inputs = args[0].split('\n')
-    msg = ""
+    messages =  []
     for input in inputs:
-        msg += hoger_lager( input, inputs[-1], len( inputs ) )
+        result = hoger_lager( input, inputs[-1], len( inputs ) )
+        messages.append( Message( result ) )
 
-    return EvaluationResult(True, expected, actual, [Message( msg +"\\n"+expected+"\\n"+actual)])
+    return EvaluationResult( True, expected, actual, messages )
