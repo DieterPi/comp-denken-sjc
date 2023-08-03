b676f6111c2ef605c89be8cfd8f744b95c0c7fd3
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-08-04 00:38:13 +0200

    Solution fix + test fix

--- 4th year/04 geavanceerd/18 piraten/solution/solution.nl.py
+++ 4th year/04 geavanceerd/18 piraten/solution/solution.nl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def piraten_puzzel( aantal_piraten = 5  ):
+def piraten_puzzel( aantal_piraten: int = 5  ) -> None :
     flag = True
     startaantal = 10
     j = 0
@@ -9,7 +9,7 @@ def piraten_puzzel( aantal_piraten = 5  ):
         aantal = startaantal + j
 
         # Simuleert het verdeelgedrag, +1 omdat op het einde iedereen wakker is en hetzelfde nog eens gebeurt
-        for i in range( aantal_piraten + 1 ):
+        for _ in range( aantal_piraten + 1 ):
             rest = aantal % aantal_piraten
             aantal = aantal // aantal_piraten * ( aantal_piraten - 1 )
             flag = flag and rest == 1
@@ -21,4 +21,4 @@ def piraten_puzzel( aantal_piraten = 5  ):
         else:
             flag = False
 
-    print( 'Bij {} piraten zijn er {} kokosnoten nodig.'.format( aantal_piraten, startaantal + j ) )
+    print( 'Bij', aantal_piraten, 'piraten zijn er', startaantal + j, 'kokosnoten nodig.' )
