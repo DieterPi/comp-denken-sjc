9519da641b1dd3e933399c86dc9edc71b20fb565
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-08-11 21:25:57 +0200

    Evaluation fix

--- rtests/04 capture recapture/evaluation/Feedback.R
+++ rtests/04 capture recapture/evaluation/Feedback.R
@@ -1,9 +1,9 @@
 context({
   testcase("Variabelen werden correct gedeclareerd.", {
     testEqual("n", function(student_env) { student_env$n }, 75)
-    testEqual("s", function(student_env) { student_env$N }, 10)
+    testEqual("s", function(student_env) { student_env$s }, 10)
   })
   testcase("Populatie werd correct berekend.", {
-    testEqual("N", function(student_env) { student_env$n }, 240)
+    testEqual("N", function(student_env) { student_env$N }, 240)
   })
 })
