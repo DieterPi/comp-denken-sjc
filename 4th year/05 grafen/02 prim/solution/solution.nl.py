00d9a1ca7fdef2f9bb64bee4034ef0490a7bde31
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-08-14 00:04:16 +0200

    Trying yaml

--- 4th year/05 grafen/02 prim/solution/solution.nl.py
+++ 4th year/05 grafen/02 prim/solution/solution.nl.py
@@ -1,51 +1,44 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import math
-
-def is_connected( vertex, edge ):
+def is_connected( vertex: str, edge: list ) -> bool:
     flag = vertex in ( edge[0], edge[1] )
     return flag
 
-def other_vertex( edge, vertex ):
+def other_vertex( edge: list, vertex: str ) -> str:
     if edge[0] == vertex:
         other_vertex = edge[1]
     else:
         other_vertex = edge[0]
     return other_vertex
 
-def MST_prim( V, E ):
-    vertex = V[0] # starting vertex, can be chosen at random
+def MST_prim( V: list, E: list ) -> int:
+    tree = []
+    
+    v1 = V[0] # starting vertex, can be chosen at random
 
-    vertex_unconnected = V
-    vertex_unconnected.remove(vertex)
-    vertex_connected = [vertex]
-    MST = []
-
-    while len(vertex_unconnected) > 0:
-        min_val = math.inf
-        for vertex in vertex_connected:
-            for edge in E:
-                if is_connected( vertex, edge ):
-                    if other_vertex( edge, vertex ) in vertex_unconnected :
-                        # this is a vertex connected to the initial vertex
-                        # now checking if it's minimal
-                        if edge[2] < min_val: 
-                            min_val = edge[2]
-                            min_edge = edge
-                            if edge[0] in vertex_connected:
-                                min_vertex = edge[1]
-                            else:
-                                min_vertex = edge[0]
+    V.remove(v1) # remaining vertices
+    connected_v = [ v1 ]
+    while len( V ) > 0:
+        min_val = 10**100
+        for v in connected_v:
+            for e in E:
+                if is_connected( v, e ) and other_vertex( e, v ) in V :
+                    # this is a vertex connected to the initial vertex
+                    # and the other endpoint hasn't been connected yet
+                    # now checking if it's minimal
+                    if e[2] < min_val: 
+                        min_val = e[2]
+                        min_edge = e
+                        if e[0] in connected_v:
+                            min_vertex = e[1]
+                        else:
+                            min_vertex = e[0]
         
         # has looped through every vertex in MST and every available edge
-        vertex_connected.append( min_vertex )
+        connected_v.append( min_vertex )
 
-        MST.append( min_edge )
+        tree.append( min_edge )
         E.remove( min_edge )
-        vertex_unconnected.remove( min_vertex )
+        V.remove( min_vertex )
     
-    return MST
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+    return tree
