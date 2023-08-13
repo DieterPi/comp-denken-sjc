dbd07c99655449c8ba2184b74e135315f728b50c
Dieter Vanderfaeillie <dieter.vanderfaeillie@gmail.com>
2023-08-13 23:00:18 +0200

    Transform to TESTed

--- 4th year/05 grafen/02 prim/preparation/generator.py
+++ 4th year/05 grafen/02 prim/preparation/generator.py
@@ -2,23 +2,12 @@ import os
 import sys
 import importlib
 import random
-import string
+import ruamel.yaml
+
+yaml = ruamel.yaml.YAML()
 
 # set fixed seed for generating test cases
 random.seed(123456789)
-
-def triangle_num(n):
-    som = 0
-    for i in range(1,n):
-        som += i
-    return som
-
-def edge_exists( E, vertex1, vertex2 ):
-    flag = False
-    for edge in E:
-        flag += (edge[0] == vertex1 and edge[1] == vertex2) or (edge[1] == vertex1 and edge[0] == vertex2)
-
-    return flag
 
 # locate evaldir
 evaldir = os.path.join('..', 'evaluation')
@@ -37,43 +26,103 @@ spec = importlib.util.spec_from_file_location(module_name, file_path)
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 
-for name in dir(module):
-    if not (name.startswith('__') and name.endswith('__')):
-        globals()[name] = eval(f'module.{name}')
+def write_yaml( data:list ):
+    """ A function to write YAML file"""
+    with open(os.path.join('..', 'evaluation', 'tests.yaml'), 'w', encoding='utf-8') as f:
+        yaml.dump(data, f)
+
+def construct_graph( connected = False, weight = 0 ) -> dict:
+    """ A function to construct a (possibly disconnected) random graph
+    The implementation is not that good xD
+    
+    :param connected: boolean indicating if there can be vertices without connections.
+    :param weight: int indicating if the graph should be weighted, if weighted then this
+                   number is used as a max for the weight.
+    """
+    alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    n_vertices = random.randint(3, 10) # can be set to 26 for huge graphs
+    V = []
+    for i in range(n_vertices):
+        V.append(alf[i])
+        
+    E = []
+    E_simple = []
+    n_edges = random.randint(n_vertices, (n_vertices - 1)*n_vertices/2 )
+    
+    # construction of all possible edges, and then picking some at random
+    all_E = []
+    for v1 in V:
+        for v2 in V:
+            if v1 != v2:
+                all_E.append( (v1, v2) )
+    
+    discount = 0
+    if connected: # if all vertices must be present in the graph
+        i = 0
+        while i < n_vertices - 1:
+            v1 = V[i]
+            v2 = random.choice( V )
+            edge_c = (v1, v2)
+            edge_c_inv = (v2, v1)
+            if v1 != v2 and edge_c not in E_simple and edge_c_inv not in E_simple:
+                E_simple.append( edge_c)
+                i += 1
+                if weight != 0:
+                    E.append( (v1, v2, random.randint(2, weight)) )
+                else:
+                    E.append( (v1, v2) )
+            else:
+                try:
+                    all_E.remove( (v1, v2) )
+                    all_E.remove( (v2, v1) )
+                except ValueError:
+                    pass
+        discount = n_vertices - 1
+    
+    i = discount
+    while i < n_edges:
+        edge_c = random.choice( all_E )
+        edge_c_inv = (edge_c[1], edge_c[0])
+        if edge_c not in E_simple and edge_c_inv not in E_simple:
+            E_simple.append( edge_c)
+            i += 1
+            if weight != 0:
+                E.append( (edge_c[0], edge_c[1], random.randint(2, weight)) )
+            else:
+                E.append( (edge_c[0], edge_c[1]) )
+        else: # remove this edge from all
+            try:
+                all_E.remove(edge_c)
+                all_E.remove(edge_c_inv)
+            except ValueError:
+                pass
+            
+    return {'V': V, 'E': E}
 
 # generate test data
-alphabet = list( string.ascii_uppercase )
-ntests= 5
+ntests = 20
 cases = [ ( ['A','B','C','D','E'], 
             [('A','B',3), ('B','C',4),('C','D',2),('B','D',7),('B','E',8)] ), ]
-while len(cases) < ntests:
-    aantal_v = random.randint(4,7)
-    V = alphabet[:aantal_v]
-    E = []
 
-    vertex_unconnected = V.copy()
-    while len(vertex_unconnected) > 0:
-        vertex1 = V[random.randint(0,aantal_v-1)]
-        vertex2 = V[random.randint(0,aantal_v-1)]
-        weight = random.randint(1, 15)
-
-        if vertex1 != vertex2 and not(edge_exists( E, vertex1, vertex2)):
-            E.append( (vertex1, vertex2, weight))
-            if(vertex1 in vertex_unconnected):
-               vertex_unconnected.remove(vertex1)
-            if(vertex2 in vertex_unconnected):
-                vertex_unconnected.remove(vertex2)
-
-    cases.append( (V, E) ) 
+while len( cases ) < ntests:
+    G = construct_graph(connected = True, weight = 20)
+    cases.append( (G['V'], G['E']) )
 
 # generate unit tests for functions
-sys.stdout = open(os.path.join('..', 'evaluation', '0.in'), 'w', encoding='utf-8')
+yamldata = []
+yamldata.append( {'tab': 'Feedback', 'testcases': []})
+# input, expression, statement or stdin?
+input = 'expression'
+# output, stdout or return?
+output = 'return'
 for test in cases:
     # generate test expression
-    print(f'>>> MST_prim( {test[0]}, {test[1]} )')
+    expression_name = 'MST_prim( {} , {} )'.format( test[0] , test[1] )
+    result = ruamel.yaml.comments.CommentedSeq( module.MST_prim( test[0], test[1] ) )
+    result.fa.set_flow_style() # to prohibit output from being YAML transformed
+    
+    # setup for return expressions
+    testcase = { input: expression_name, output: result }
+    yamldata[0]['testcases'].append( testcase)
 
-    # generate return value
-    try:
-        print('{}'.format(module.MST_prim( test[0], test[1])))
-    except Exception as e:
-        print('Traceback (most recent call last):\n{}: {}'.format(e.__class__.__name__, e))
+write_yaml(yamldata)
