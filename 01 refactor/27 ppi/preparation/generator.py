dfebf25eeb4bf0cfa73166fdb1c7d8186874edfe
dvanderfaeillie <dieter.vanderfaeillie@gmail.com>
2025-10-27 20:16:11 +0100

    Refactor

---
+++ 01 refactor/27 ppi/preparation/generator.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import importlib.util
+import random
+import ruamel.yaml
+import subprocess
+
+yaml = ruamel.yaml.YAML()
+
+# set fixed seed for generating test cases
+random.seed(12345678)
+
+# locate evaldir
+evaldir = os.path.join('..', 'evaluation')
+if not os.path.exists(evaldir):
+    os.makedirs(evaldir)
+
+# locate solutiondir
+solutiondir = os.path.join('..', 'solution')
+if not os.path.exists(solutiondir):
+    os.makedirs(solutiondir)
+
+def write_yaml( data:list ):
+    """ A function to write YAML file"""
+    with open(os.path.join('..', 'evaluation', 'tests.yaml'), 'w', encoding='utf-8') as f:
+        yaml.dump(data, f)
+
+
+module_name = 'solution'
+file_path = os.path.join(solutiondir, 'solution.nl.py')
+spec = importlib.util.spec_from_file_location(module_name, file_path)
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+
+# generate test data
+ntests= 20
+cases = [(1754,2480,300)]
+ppi = (72,150,200,300,600)
+resol = [(2560, 1920),(4000,3000),(3840,2160),(1600,1200),(2592,1944),(7360,4912),(4256,2848)]
+while len(cases) < ntests:
+    r = random.randint(0,len(resol)-1)
+    case = (resol[r][0], resol[r][1], ppi[random.randint(0, len(ppi)-1)])
+    if case not in cases:
+        cases.append(case)
+
+#cases.sort()
+
+# generate unit tests for functions
+yamldata = []
+
+# input, expression, statement or stdin?
+input = 'stdin'
+# output, stdout or return?
+output = 'stdout'
+tabtitle = "Feedback"
+
+yamldata.append( {'tab': tabtitle, 'contexts': []})
+
+for i in range(len(cases)):
+    test = cases[i]
+    yamldata[0]['contexts'].append( {'testcases' : []})
+    # generate test expression
+    # add input to input file
+    stdin = '\n'.join(f'{line}' for line in test)
+
+    # generate output to output file
+    script = os.path.join(solutiondir, 'solution.nl.py')
+    process= subprocess.run(
+        ['python3', script],
+        input=stdin,
+        encoding='utf-8',
+        capture_output=True
+    )
+    
+    result_lines = process.stdout.split("\n")
+    result_lines = [x for x in result_lines[:-1]] ## drop last element
+    
+    outputtxt = ""
+    for line in result_lines:
+        if not(line.startswith( 'Geef' ) or line.startswith( 'Voer' )):
+            print(line)
+            outputtxt += line + "\n"
+            
+    testcase = { input: stdin, output: outputtxt }            
+    yamldata[0]['contexts'][i]["testcases"].append( testcase)
+
+
+write_yaml(yamldata)
--- 01 refactor/27 ppi/solution/solution.nl.py
+++ 01 refactor/27 ppi/solution/solution.nl.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 b = int( input( 'Geef de breedte in:' ) )
 h = int( input( 'Geef de hoogte in:' ) )
 ppi = int( input( 'Geef de dichtheid in'))
@@ -8,5 +5,4 @@ ppi = int( input( 'Geef de dichtheid in'))
 nb = round( b / ppi * 2.54, 3)
 nh = round( h / ppi * 2.54, 3)
 
-print('')
 print('Deze foto kan je afdrukken op', nb, 'x', nh, 'cm.')
