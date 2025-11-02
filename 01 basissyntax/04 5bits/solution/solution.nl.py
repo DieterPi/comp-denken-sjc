0c0d2271565899d3e6dc4e344195f8ea51fbc03e
dvanderfaeillie <dieter.vanderfaeillie@gmail.com>
2025-11-02 15:26:31 +0100

    binary to TESTed

--- 01 basissyntax/04 5bits/solution/solution.nl.py
+++ 01 basissyntax/04 5bits/solution/solution.nl.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 binair = int( input( 'Geef de getal in (van 5 bits): ' ) )
 
 TD = binair // 10000
@@ -19,5 +16,4 @@ E = T_rest
 
 getal = TD * pow( 2, 4 ) + D * pow( 2, 3 ) + H * pow( 2, 2 ) + T * 2 + E
 
-print('')
 print('Dit is', getal,'in het decimale talstelsel')
